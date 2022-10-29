ea9e89d497233ec930f505188c2b9367adf988b2
Maks Kayun <maks.kayun@yandex.ru>
2022-10-30 00:16:44 +0300

    рефакторинг

--- Auto.PricingEngine/Services/VehiclePricingService.cs
+++ Auto.PricingEngine/Services/VehiclePricingService.cs
@@ -3,38 +3,37 @@ using Grpc.Core;
 
 namespace Auto.PricingEngine.Services;
 
-public class PricerService : Pricer.PricerBase {
-    private readonly ILogger<PricerService> logger;
+public class PricerService : Pricer.PricerBase
+{
+    private readonly ILogger<PricerService> _logger;
     private readonly IDataRequestService _service;
+
     public PricerService(ILogger<PricerService> logger, IDataRequestService service)
     {
-        this.logger = logger;
+        _logger = logger;
         _service = service;
     }
+
     public override Task<PriceReply> GetPrice(PriceRequest request, ServerCallContext context)
     {
-        if (TryFindModel(request.Model))
-        {
-            if (request.Manufacturer.StartsWith("m"))
-                return Task.FromResult(new PriceReply
-                {
-                    Price = new Random().Next(5_000_000, 900_000_000),
-                    CurrencyCode = "RUB"
-                });
-        
+        if (!TryFindModel(request.Model))
             return Task.FromResult(new PriceReply
             {
-                Price = new Random().Next(1_000_000, 5_000_000),
+                Price = -1,
+                CurrencyCode = "Такой модели нет"
+            });
+        if (request.Manufacturer.StartsWith("m"))
+            return Task.FromResult(new PriceReply
+            {
+                Price = new Random().Next(5_000_000, 900_000_000),
                 CurrencyCode = "RUB"
             });
-        }
-        
+
         return Task.FromResult(new PriceReply
         {
-            Price = -1,
-            CurrencyCode = "Такой модели нет"
+            Price = new Random().Next(1_000_000, 5_000_000),
+            CurrencyCode = "RUB"
         });
-
     }
 
     private bool TryFindModel(string byModel) => _service.GetModel(byModel) != null;
