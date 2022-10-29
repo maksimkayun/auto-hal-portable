892390543b0892dc8c818e726c77b53cdef47a1a
Maks Kayun <maks.kayun@yandex.ru>
2022-10-29 16:47:07 +0300

    add gRPC

---
+++ Auto.PricingClient/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Auto.PricingEngine;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7102");
+var grpcClient = new Pricer.PricerClient(channel);
+Console.WriteLine("Ready! Press any key to send a gRPC request (or Ctrl-C to quit):");
+while (true) {
+    Console.ReadKey(true);
+    var request = new PriceRequest {
+        Model = "volkwsagen-beetle",
+        Color = "Green",
+        Year = 1985
+    };
+    var reply = grpcClient.GetPrice(request);
+    Console.WriteLine($"Price: {reply.Price}");
+}
---
+++ Auto.PricingEngine/Services/VehiclePricingService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+
+namespace Auto.PricingEngine.Services;
+
+public class PricerService : Pricer.PricerBase {
+    private readonly ILogger<PricerService> logger;
+    public PricerService(ILogger<PricerService> logger) {
+        this.logger = logger;
+    }
+    public override Task<PriceReply> GetPrice(PriceRequest request, ServerCallContext context) {
+        return Task.FromResult(new PriceReply() { CurrencyCode = "RUB", Price = 400 });
+    }
+}
